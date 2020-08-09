f08364463683469ae3b4de84fc4cbbb8d6e102f6
骆佳俊 <1104772543@qq.com>
2020-08-09 21:48:09 +0800

    'dailyLeetcode'

--- 面经/leetcode/125.验证回文串.js
+++ 面经/leetcode/125.验证回文串.js
@@ -16,7 +16,7 @@ var isPalindrome = function (s) {
     }
     let left = 0
     let right = s.length - 1
-    while (left < right) {
+    while (left < right) { 
         if (s[left] != s[right]) {
             return false
         }
